0e64746180da9b47c83049ea97d8b1922d118d0d
Ashan Malinda Perera <ashanp@gmail.com>
2024-08-31 19:18:18 +1200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,6 +16,16 @@ var userIdvalue = context.Request.Headers["ID"];
 await context.Response.WriteAsync($"<div> Key: {userIdkey} </div>");
 await context.Response.WriteAsync($"<div> value: {userIdvalue} </div>");
 
-  
+// this way we will not be able to accsess the data. so we need to use a streamreader 
+var requestBody = context.Request.Body;
+await context.Response.WriteAsync($"<div> body: {requestBody} </div>");
+
+// this is how we access a stream to get the data using stream reader
+String requestBodyString;
+using(StreamReader reader = new StreamReader(context.Request.Body)){
+    requestBodyString = await reader.ReadToEndAsync();
+    await context.Response.WriteAsync($"<div> the body is : {requestBodyString} </div>");
+}
+
 });
 app.Run();
